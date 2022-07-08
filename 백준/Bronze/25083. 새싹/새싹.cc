82a78308238ce66a63250acc553f8a5d1d6963b5
thlee00 <56151577+thlee00@users.noreply.github.com>
2022-07-09 03:49:57 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout <<
+		"         ,r'\"7" << endl <<
+		"r`-_   ,'  ,/" << endl <<
+		" \\. \". L_r'" << endl <<
+		"   `~\\/" << endl <<
+		"      |" << endl <<
+		"      |";
+}
