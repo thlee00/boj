489a84662ef89f0f5e81a182f24f3c43098b860c
thlee00 <56151577+thlee00@users.noreply.github.com>
2022-07-30 05:03:39 +0900

    [Silver III] Title: N과 M (4), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+int n, m;
+int ans[9] = { 0 };
+int visited[9] = { 0 };
+
+void func(int idx, int num) {
+	if (num == m) {
+		for (int i = 0; i < m - 1; i++)
+			cout << ans[i] << " ";
+		cout << ans[m - 1] << "\n";
+		return;
+	}
+
+	for (int i = idx + 1; i <= n; i++) {
+		ans[num] = i;
+		func(i - 1, num + 1);
+	}
+	return;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cin >> n >> m;
+
+	func(0, 0);
+
+	return 0;
+
+}
