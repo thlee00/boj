74d2a45fc3564087363c8fbae2a3bae59e50db2f
Taehui Lee <thlee00@postech.ac.kr>
2023-07-24 21:09:56 +0900

    [level 4] Title: 도둑질, Time: 5.83 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.cpp
@@ -9,26 +9,20 @@ int dp[1000001];
 
 int solution(vector<int> money) {
     int answer = 0;
+    int cnt = 2;
     
-    dp[0] = money[0];
-    dp[1] = max(dp[0], money[1]);
-    
-    for (int i = 2; i < money.size() - 1; i++) {
-        dp[i] = max(dp[i - 1], dp[i - 2] + money[i]);
+    while(cnt--) {
+        dp[0] = money[cnt];
+        dp[1] = max(dp[0], money[cnt + 1]);
+
+        for (int i = 2; i < money.size() - 1; i++) {
+            dp[i] = max(dp[i - 1], dp[i - 2] + money[i + cnt]);
+        }
+
+        answer = max(answer, dp[money.size() - 2]);
+
+        fill(dp, dp + 1000001, 0);
     }
-    
-    answer = dp[money.size() - 2];
-    
-    fill(dp, dp + 1000001, 0);
-    
-    dp[0] = money[1];
-    dp[1] = max(dp[0], money[2]);
-    
-    for (int i = 2; i < money.size() - 1; i++) {
-        dp[i] = max(dp[i - 1], dp[i - 2] + money[i + 1]);
-    }
-    
-    answer = max(answer, dp[money.size() - 2]);
     
     return answer;
 }
